7161445732bf305a34379fd030c7750f5cd11234
blumoc9 <blumxp@gmail.com>
2013-12-16 11:21:03 -0430

    filtros

---
+++ Fernando/src/com/sigarep/modelo/ProfesorFiltros.java
@@ -0,0 +1,37 @@
+package com.sigarep.modelo;
+
+public class ProfesorFiltros {
+	private String nombre="",apellido="",email="",sexo="";
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre==null?"":nombre.trim();//revisar
+	}
+//revisar esto
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido==null?"":apellido.trim();
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo==null?"":sexo.trim();
+	}
+}
